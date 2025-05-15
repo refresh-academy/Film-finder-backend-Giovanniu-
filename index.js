54d98053c51ad2ba175b41c332f8a1ca231e8d47
Giovani-stack <giovanni.panziera@refresh-academy.org>
2025-05-15 10:50:41 +0200

    added package cowsay

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 console.log("Hello wordl");
+import cowsay from 'cowsay';
+console.log(cowsay.say({
+    text : "I'm a moooodule",
+    e : "oO",
+    T : "U "
+}));
+import yosay from 'yosay';
+console.log(yosay('Hello, and welcome to my fantastic generator full of whimsy and bubble gum!'));
+import express from 'express'
+let app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.listen(3000)
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
